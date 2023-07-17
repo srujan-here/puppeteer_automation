f001434e06a1f1403026686e3f0e94038f399441
Srujan <puttasrujan1212@gmail.com>
2023-07-17 21:11:11 +0530

    completed all the functionalities

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function srujan() {
   // Launch headful browser
@@ -8,22 +8,55 @@ async function srujan() {
   const page = await browser.newPage();
 
   // Go to the website
-  await page.goto('https://swap.defillama.com/');
+  await page.goto("https://swap.defillama.com/");
 
   // Fill the form
-  await page.type('#chain', 'Arbitrum One');
-  await page.type('#sellInput', '12');
-  await page.type('#sellTokenSelect input', 'WBTC');
-  await page.type('#buyTokenSelect input', 'USDC');
 
-  // Wait for the swap options to appear
-  await page.waitForSelector('.swap-options');
+  // Set the new content for a specific div element using evaluate()
+  await page.evaluate(() => {
+    const divElement = document.querySelector(
+      "#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-0.bVZjlY > div:nth-child(1) > div > div > div.css-0"
+    );
+    divElement.textContent = "Arbitrum One"; // Set the new content
+  });
 
-  // Select the second option
-  await page.click('.swap-options li:nth-child(2)');
+  // Type a new value in an input field
+  const xpath1 = '//*[@id="__next"]/div/div/div[2]/main/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div/input';
+  const inputElement1 = await page.waitForXPath(xpath1);
+  await inputElement1.click({ clickCount: 3 }); // Select existing value (optional)
+  await inputElement1.type("12", { delay: 100 }); // Type the new value
 
-  // Leave the browser window open
-  // await browser.close();
+  // Click a button
+  await page.click(
+    "#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-0.bVZjlY > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > button",
+  );
+
+  // Type a value in an input field
+  await page.type(".chakra-input", "WBTC", { delay: 500 });
+
+  // Click the first option in a dropdown list
+  const divSelector1 = '#chakra-modal-\\:rd\\: > div.List > div > div:first-child';
+  await page.click(divSelector1);
+
+  // Click a button
+  await page.click(
+    "#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-0.bVZjlY > div.css-1urcov8 > div:nth-child(3) > div.css-1k491an > button ",
+    { delay: 300 }
+  );
+
+  // Type a value in an input field
+  await page.type(".chakra-input", "USDC", { delay: 500 });
+
+  // Click the Second option in a dropdown list
+  // const divSelector2 = '#chakra-modal-\\:r1n\\: > div.List > div > div:nth-child(2)';
+  // await page.click(divSelector2);
+  
+
+  // Wait for a specific amount of time
+  await page.waitForTimeout(7000); // Wait for 7 seconds (7000 milliseconds)
+
+  // Click a button
+  await page.click('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-2.cObIGF > div:nth-child(5) > div:nth-child(1)');
 }
 
 srujan();
